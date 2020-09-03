eb7776aa74c40e871c5f30be3893fdfe4f89ec92
onilinkman <marbanchristian@gmail.com>
2020-09-03 01:04:53 -0400

    se mejoro la respuesta del cerdito hacia los ataques

--- Enemigos/cerdo/CerdoEnemigo.gd
+++ Enemigos/cerdo/CerdoEnemigo.gd
@@ -38,7 +38,7 @@ func cambiarTransicion_a(nuevo_estado):
 
 
 func guardarMovimientoEnX():
-	if moverX!=Vector2(0,0):
+	if moverX!=Vector2(0,0) :
 		saveMoveX=moverX
 		moverX=Vector2(0,0)
 		
@@ -83,7 +83,7 @@ func _physics_process(delta):
 			$duracionParado.stop()
 	elif deteccionPiso==null and esEnPiso:
 		cambiarDireccionMovimiento()
-	elif !esEnPiso and moverX!=saveMoveX:
+	elif !esEnPiso and moverX!=saveMoveX and estado!=DANIO:
 		cambiarTransicion_a(CAIDA)
 	
 	
@@ -94,26 +94,31 @@ func _physics_process(delta):
 	
 	posicion.x=posicion.x*velocidad_movimiento
 	
-	if estado==DANIO:
-		posicion.x*=-1
-	
 	move_and_slide(posicion,Vector2(0,-1))
 
 
 
-func hacerDanio()->void:
+func hacerDanio(var ejeX)->void:
 	if estado!=DANIO:
 		$caminar.stop()
 		$duracionParado.stop()
 		$duracionAtaque.stop()
 		estaAtacando=false
 		cambiarTransicion_a(DANIO)
-		$hitbox/CollisionShape2D.disabled=true
+		$hitbox/CollisionShape2D.set_deferred("disabled",true)
+		var direccion=global_position.x-ejeX
+		var sentido=direccion/abs(direccion)
+		moverX=Vector2(sentido,0)
+		saveMoveX=-1*moverX
+		
 	
 	
 
 func cambiarDireccionMovimiento():
 	moverX*=-1
+	ajustarFrente()
+
+func ajustarFrente():
 	if moverX.x>0 or (saveMoveX.x>0 and moverX.x==0):
 		$detectarFrente.rotation_degrees=270
 		$detectarFrente/detectarPiso.rotation_degrees=90
@@ -124,6 +129,7 @@ func cambiarDireccionMovimiento():
 		$detectarFrente/detectarPiso.rotation_degrees=-90
 		$AnimatedSprite.flip_h=false
 		$hitbox.position.x=-9
+	
 
 func launchRay(var vect):
 	var estadoEspacial=get_world_2d().direct_space_state
@@ -170,7 +176,7 @@ var numGolpes=0
 
 func _on_hitbox_body_exited(body):
 	#estaAtacando=false
-	$hitbox/CollisionShape2D.disabled=true
+	$hitbox/CollisionShape2D.set_deferred("disabled",true)
 	emit_signal("darGolpe")
 
 
@@ -184,9 +190,10 @@ func _on_AnimatedSprite_animation_finished():
 		ATACAR:
 			cambiarTransicion_a(PARADO)
 			estaAtacando=false
-			$hitbox/CollisionShape2D.disabled=true
+			$hitbox/CollisionShape2D.set_deferred("disabled",true)
 			$duracionAtaque.stop()
 			$duracionParado.start(rand_range(3,5))
 		DANIO:
 			cambiarTransicion_a(CAMINAR)
 			$caminar.start(rand_range(8,14))
+			ajustarFrente()
